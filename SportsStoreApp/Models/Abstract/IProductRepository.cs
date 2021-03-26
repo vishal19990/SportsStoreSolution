a8db27f198b55a471d3192e38e8585c700d046e3
vishal19990 <vishalbangera98@gmail.com>
2021-03-26 15:32:23 +0530

    new changes

---
+++ SportsStoreApp/Models/Abstract/IProductRepository.cs
@@ -0,0 +1,19 @@
+﻿using SportsStoreApp.Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SportsStoreApp.Models.Abstract
+{
+   public  interface IProductRepository
+    {
+        Task<Product> AddProductAsync(Product product);
+        Task<Product> UpdateProductAsync(Product product);
+
+        Task<bool> DeleteProductAsync(int productId);
+        Task<IEnumerable<Product>> GetProductsAsync();
+        Task<IEnumerable<Product>> GetProductsByCategoryAsync(string category);
+        Task<Product> GetProductByIdAsync(int productId);
+    }
+}
---
+++ SportsStoreApp/Models/Concrete/EFProductRepository.cs
@@ -0,0 +1,66 @@
+﻿using SportsStoreApp.Models.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SportsStoreApp.Models.Entities;
+using Microsoft.Extensions.Logging;
+using Microsoft.EntityFrameworkCore;
+
+namespace SportsStoreApp.Models.Concrete
+{
+    public class EFProductRepository : IProductRepository
+    {
+        private SportsStoreDbcontext _context;
+        private ILogger<EFProductRepository> _logger;
+
+        public EFProductRepository(SportsStoreDbcontext sportsStoreDbcontext,ILogger<EFProductRepository> logger)
+        {
+            _context = sportsStoreDbcontext;
+            _logger = logger;
+        }
+        public async Task<Product> AddProductAsync(Product product)
+        {
+          await  _context.Products.AddAsync(product);
+            var recEffected = await _context.SaveChangesAsync();
+            if(recEffected==1)
+            {
+                _logger.LogInformation($"EfProductRepository .add product async ,new product{product.ProductName},added successfully");
+                return product;
+            }
+            return null;
+        }
+
+        public async Task<bool> DeleteProductAsync(int productId)
+        {
+            var product = await GetProductByIdAsync(productId);
+            _context.Products.Remove(product);
+            var recEffected = await _context.SaveChangesAsync();
+            if (recEffected == 1)
+            {
+                _logger.LogInformation($"EfProductRepository.deleteproductasync, add product  ,new product{product.ProductId},deleted successfully");
+                return true;
+            }
+            return false;
+        }
+
+        public async Task<Product> GetProductByIdAsync(int productId) => await _context.Products.FindAsync(productId);
+
+        public async Task<IEnumerable<Product>> GetProductsAsync() => await _context.Products.ToListAsync();
+
+
+        public async Task<IEnumerable<Product>> GetProductsByCategoryAsync(string category) => await _context.Products.Where(p => p.Category == category).ToListAsync();
+        
+
+        public async Task<Product> UpdateProductAsync(Product product)
+        {
+            _context.Entry<Product>(product).State = EntityState.Modified;
+            var recEffected = await _context.SaveChangesAsync();
+            if(recEffected==1)
+            {
+                _logger.LogInformation($"EfProductRepository.Updatedproductasync, product with productidt{product.ProductId},Updated successfully");
+            }
+            return product;
+        }
+    }
+}
