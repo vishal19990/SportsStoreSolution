5a639876e6a4e23e29b658d63f43613075e7d7dc
Vishal <vishal@aykansoft.com>
2022-03-12 18:47:05 +0530

    New design updated

--- SportsStoreApp/ClientApp/src/app/app.module.ts
+++ SportsStoreApp/ClientApp/src/app/app.module.ts
@@ -12,10 +12,20 @@ import {RouterModule} from '@angular/router'
 import { StoreComponent } from './stores/store.component';
 import { CartDetailComponet } from './stores/cartDetails.component';
 import {  CheckoutComponent} from "./stores/checkout.component";
-import {StoreFirstGuard }from "./storeFirst.guard"
+import {StoreFirstGuard }from "./storeFirst.guard";
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { ContactComponent } from './contact/contact.component';
+import { ContactListComponent } from './contact-list/contact-list.component';
+import { LandingPageComponent } from './landing-page/landing-page.component'
 @NgModule({
   declarations: [
     AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    ContactComponent,
+    ContactListComponent,
+    LandingPageComponent,
     // StoreComponent
     //NavFooterComponent,NavHeaderComponent
   ],
@@ -24,7 +34,12 @@ import {StoreFirstGuard }from "./storeFirst.guard"
     RouterModule.forRoot([
     {path: 'store', component:StoreComponent,canActivate:[StoreFirstGuard]},
     {path: 'cart', component: CartDetailComponet,canActivate:[StoreFirstGuard]},
+    {path: 'landing', component: LandingPageComponent,canActivate:[StoreFirstGuard]},
+
     {path: 'checkout', component:CheckoutComponent,canActivate:[StoreFirstGuard]},
+    {path: 'login', component:LoginComponent,canActivate:[StoreFirstGuard]},
+    {path: 'contactList', component:ContactListComponent,canActivate:[StoreFirstGuard]},
+    {path: 'register', component:RegisterComponent,canActivate:[StoreFirstGuard]},
     {path: 'admin' ,loadChildren:()=>import ('./admin/admin.module').then(l=>l.AdminModule)},
     {path: '**', redirectTo: '/store'}
   ])
--- SportsStoreApp/ClientApp/src/app/nav/navfooter.componet.ts
+++ SportsStoreApp/ClientApp/src/app/nav/navfooter.componet.ts
@@ -6,13 +6,13 @@ import { Component, Input } from '@angular/core';
 })
 
 export class NavFooterComponent {
-  getDateTime = () => {
-    const date = new Date();
-    const options = {
-      year: 'numeric', month: 'short', day: 'numeric', weekday: 'short',
-      hour: '2-digit', minute: '2-digit', second: '2-digit'
-    };
-    return date.toLocaleTimeString('en-IN', options);
-  }
+  //getDateTime = () => {
+  //  const date = new Date();
+  //  const options = {
+  //    year: 'numeric', month: 'short', day: 'numeric', weekday: 'short',
+  //    hour: '2-digit', minute: '2-digit', second: '2-digit'
+  //  };
+  //  return date.toLocaleTimeString('en-IN', options);
+  //}
 }
 
